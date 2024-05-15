238823890d0cb0c4c8a9fd4847d3953166b32b31
emads22 <emadsaab722@gmail.com>
2024-05-15 11:04:41 +0300

    add clear button and signature

--- config.py
+++ config.py
@@ -4,13 +4,31 @@ from pathlib import Path
 ASSETS_DIR = Path("./assets")
 DEFINITIONS_FILE = ASSETS_DIR / "data" / "data.json"
 
-APP_STYLE_SHEET = """
+APP_STYLESHEET = """
 background-color: yellow;
 font-family: 'Courier New';
 font-size: 12pt;
 """
 
-BUTTON_1_STYLE_SHEET = """
+TITLE_STYLESHEET = """
+font-size: 30px;
+font-weight: bold;
+color: #333333; 
+margin: 10px; 
+text-align: center;  
+"""
+
+BUTTON1_STYLESHEET = """
 background-color: pink;
 font-weight: bold;
+"""
+
+BUTTON2_STYLESHEET = """
+background-color: orange;
+font-weight: bold;
+"""
+
+SIGNATURE_STYLESHEET = """
+font-family: 'Arial';
+font-size: 10pt;
 """
--- main.py
+++ main.py
@@ -4,39 +4,53 @@ from PyQt6.QtWidgets import (QApplication, QMainWindow, QGridLayout,
                              QWidget, QLabel, QTextEdit, QLineEdit, 
                              QPushButton)
 from PyQt6.QtCore import Qt
-from config import DEFINITIONS_FILE, APP_STYLE_SHEET, BUTTON_1_STYLE_SHEET
+from config import DEFINITIONS_FILE, APP_STYLESHEET, BUTTON1_STYLESHEET, BUTTON2_STYLESHEET, TITLE_STYLESHEET
 
 
 # Define a custom main window class that inherits from QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Word Definition")        
+        self.setWindowTitle("WordWise")        
 
-        self.setStyleSheet(APP_STYLE_SHEET)
+        self.setStyleSheet(APP_STYLESHEET)
 
         self.definitions = self.load_data()
 
         # Create a grid layout
         layout = QGridLayout()
 
+        title = QLabel("WordWise")
+        title.setStyleSheet(TITLE_STYLESHEET)
+
         self.text_input = QLineEdit()
         self.text_input.returnPressed.connect(self.define_word)
 
 
-        button = QPushButton("Define")
-        button.setToolTip("Define")
-        button.setFixedWidth(100)
-        button.setStyleSheet(BUTTON_1_STYLE_SHEET)
-        button.clicked.connect(self.define_word)
+        button1 = QPushButton("Define")
+        button1.setToolTip("Define")
+        # button1.setFixedWidth(100)
+        button1.setStyleSheet(BUTTON1_STYLESHEET)
+        button1.clicked.connect(self.define_word)
+
+        button2 = QPushButton("Clear")
+        button2.setToolTip("Clear")
+        # button2.setFixedWidth(100)
+        button2.setStyleSheet(BUTTON2_STYLESHEET)
+        button2.clicked.connect(self.clear_all)
 
         self.text_output = QTextEdit()
         self.text_output.setReadOnly(True)
 
+        signature = QLabel("© 2024 E>")
+
         # Add widgets to the layout
-        layout.addWidget(self.text_input, 0, 0)
-        layout.addWidget(button, 0, 1)
-        layout.addWidget(self.text_output, 1, 0, 1, 2)  # Spans two columns
+        layout.addWidget(title, 0, 0, 1, 3, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.text_input, 1, 0)
+        layout.addWidget(button1, 1, 1)
+        layout.addWidget(button2, 1, 2)
+        layout.addWidget(self.text_output, 2, 0, 1, 3)  # Spans three columns
+        layout.addWidget(signature, 3, 2)
 
         # Create a main widget and set the layout
         main_widget = QWidget()
@@ -66,6 +80,12 @@ class MainWindow(QMainWindow):
             else:
                 for result in results:
                     print(result)
+        else:
+            print("No input for definition search.")
+
+    def clear_all(self):
+        self.text_input.clear()
+        self.text_output.clear()
         
 
 
